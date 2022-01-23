0d7cae6c0687345fbfabc1da1807c2d1ec0c58cc
cloud <73915583+cloudpanda1@users.noreply.github.com>
2022-01-23 12:35:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const promptEl = document.getElementById("prompt-para");
 const themeDiv = document.getElementById("nav");
 
-const prompts = ["Test1", "Test2", "Test3", "Test4", "Test5"];
+const prompts = [
+  "Think you are various kitchen appliances.", 
+  "Just got out of a coma that you were stuck in for 50 years.", 
+  "Just got out of a walk-in freezer that you were stuck in all day.", 
+  "You have a really short attention span.",
+  "Think you are the host.",
+  "Copy other peoples moods, behaviors, etc.",
+  "Baymax or another robot.",
+  "Start the party old and become incresingly young as time goes on.",
+  "Become increaingly drunk as time goes on.",
+  "Annoyed cat (without meowing)",
+  "Humming bird that thinks the other people are flowers",
+  "Scientist testing people's annoyance levels",
+  "Think that everyone around you is an alien trying to destroy humanity.",
+  "Think everything is part of a musical.",
+  "Try to hypnotise people randomly and think it works even though it doesn't",
+  "Gradually become more anxious as the party continues.",
+  "Someone possessed by a demon."];
 
 function generatePrompt() {
-    let num = Math.floor(Math.random() * 5);
+    let length = prompts.length;
+    let num = Math.floor(Math.random() * length);
     if (promptEl.textContent == prompts[num]) {
         generatePrompt();
     } else {
