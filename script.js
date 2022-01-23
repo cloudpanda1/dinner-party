4d1180ff64421953babba684960b342f40d429b6
cloud <73915583+cloudpanda1@users.noreply.github.com>
2022-01-22 22:47:46 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const promptEl = document.getElementById("prompt-para");
+const themeDiv = document.getElementById("nav");
+
+const prompts = ["Test1", "Test2", "Test3", "Test4", "Test5"];
+
+function generatePrompt() {
+    let num = Math.floor(Math.random() * 5);
+    if (promptEl.textContent == prompts[num]) {
+        generatePrompt();
+    } else {
+        promptEl.textContent = prompts[num];
+        console.log("Prompt recorded successfully.");
+    }
+}
+
+function changeTheme() {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+}
